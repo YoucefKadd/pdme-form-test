67e4c38ee327351fa19d223b8e1e405822fb5425
Youcef k <youssefkaddour@hotmail.com>
2023-07-10 17:54:07 +0200

    pdme-form-test

---
+++ src/Models/MockQuestions.js
@@ -0,0 +1,18 @@
+export const QUESTIONS = [
+    {
+        id: 1,
+        quest: "Dans quelle entreprise travaillez-vous ?",
+        respType: 'text'
+    },
+    {
+        id: 2,
+        quest: "Quelle est l’importance de votre trajet Domicile – Travail pour vous dans la réussite d’une bonne journée de travail ?",
+        respType: 'radio',
+        respList: [1, 2, 3, 4, 5]
+    },
+    {
+        id: 3,
+        quest: "Quel est le code postal de votre résidence ?",
+        respType: 'number',
+    },
+]
